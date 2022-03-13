4da346e1d6737b9e35d1b68a95d7d72e31edfc36
EMT119 <austinarbaugh16@gmail.com>
2022-03-13 10:13:23 -0400

    Unit order script

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-document.getElementById("")
+let unitOne = document.getElementById("unitone");
+let unitTwo = document.getElementById("unittwo");
+
+let unitOrder = {
+    first: localStorage.getItem("firstOut"),
+    second: localStorage.getItem("secondOut")
+}
+
+
+setInterval(() => {
+    (unitOne.innerText != unitOrder.first) ? document.getElementById("saveRow").style.display = "table-row" : null;
+    (unitTwo.innerText != unitOrder.second) ? document.getElementById("saveRow").style.display = "table-row" : null;
+}, 5000);
