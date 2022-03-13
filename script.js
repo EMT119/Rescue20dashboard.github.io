4ebeae82bbbded19aa5d02ad392f47e01b6edd98
Austin Arbaugh <austinarbaugh16@gmail.com>
2022-03-13 14:28:32 +0000

    Updated unit order script

--- script.js
+++ script.js
@@ -10,4 +10,20 @@ let unitOrder = {
 setInterval(() => {
     (unitOne.innerText != unitOrder.first) ? document.getElementById("saveRow").style.display = "table-row" : null;
     (unitTwo.innerText != unitOrder.second) ? document.getElementById("saveRow").style.display = "table-row" : null;
-}, 5000);
+    if (unitOne.innerText == unitOrder.first && unitTwo.innerText == unitOrder.second) {
+      document.getElementById("saveRow").style.display = "none";
+    }
+}, 1500);
+
+function saveChanges() {
+  let passcode = prompt("Please enter passcode");
+  if (passcode == 1967) {
+     unitOrder.first = unitOne.innerText;
+  unitOrder.second = unitTwo.innerText;
+  document.getElementById("saveRow").style.display = "none"; 
+  } else {
+    alert("Incorrect passcode");
+    unitOne.innerText = unitOrder.first;
+    unitTwo.innerText = unitOrder.second;
+  }
+}
